78bd23e970b03a28ecd7843687991adc05348484
Martin Novysedlak <martin.novysedlak.2@student.tuke.sk>
2022-01-05 19:04:14 +0100

    npod finished

--- hubbletracker/src/components/NPOD.js
+++ hubbletracker/src/components/NPOD.js
@@ -1,93 +1,87 @@
 import React, {useEffect, useState} from 'react';
-import { View, Image, Text, StyleSheet, ScrollView } from 'react-native';
+import {View, Image, Text, StyleSheet, ScrollView, Linking} from 'react-native';
 import {Component} from 'react/cjs/react.production.min';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
 export default class NPOD extends Component {
-    // state = {
-    //     copyright: '',
-    //     date: '',
-    //     explanation: '',
-    //     hdurl: '',
-    //     title: '',
-    //     url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fanatomised.com%2Fwp-content%2Fuploads%2F2016%2F05%2Fspinner-test4.gif&f=1&nofb=1',
-    // };
-
     state = {
-        copyright: '',
+        title: '',
         date: '',
+        copyright: '',
+        url: 'https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif',
         explanation: '',
         hdurl: '',
-        title: '',
-        url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fanatomised.com%2Fwp-content%2Fuploads%2F2016%2F05%2Fspinner-test4.gif&f=1&nofb=1',
     };
 
     constructor() {
         super();
-        // this.storeData('apod', JSON.stringify(this.state))
         this.init();
-        // this.pictureURL = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fanatomised.com%2Fwp-content%2Fuploads%2F2016%2F05%2Fspinner-test4.gif&f=1&nofb=1';
     }
 
     replacer(value) {
         return value.replace(/\\/g, '');
-     }
+    }
 
     async init() {
         await this.fetchPicture();
         this.updateURL(this.state);
     }
 
-    async updateURL(e){
-        this.state.url = await this.getURL()
-        this.forceUpdate(); // setState
-
+    async updateURL(e) {
+        this.state.title = await this.getTitle();
+        this.state.date = await this.getDate();
+        this.state.copyright = await this.getCopyright();
+        this.state.url = await this.getURL();
+        this.state.explanation = await this.getExplanation();
+        this.state.hdurl = await this.getHDURL();
+        this.forceUpdate();
     }
 
-    async storeData(key, value){
+    async storeData(key, value) {
         try {
-            console.debug('data value to store: '+ JSON.stringify(value))
-            const storedKey = await AsyncStorage.getItem(key)
-            console.log('stored key: ' + JSON.stringify(storedKey).replace(/\\/g, ''))
-            if (storedKey){
-                console.debug('mergujem')
+            const storedKey = await AsyncStorage.getItem(key);
+            if (storedKey) {
                 await AsyncStorage.mergeItem(key, JSON.stringify(value));
-            }
-            else {
-                console.debug('setujem')
+            } else {
                 await AsyncStorage.setItem(key, JSON.stringify(value));
             }
         } catch (e) {
-            console.error('error storing data' + e)
+            console.error('error storing data' + e);
         }
     }
 
-    async getData(key){
+    async getData(key) {
         try {
             const value = await AsyncStorage.getItem(key);
-            console.log("VALUE: " + typeof value)
-            // console.log('getData value: ' + value)
-            return JSON.parse(value)
+            return JSON.parse(value);
         } catch (e) {
-            console.error('error getting data' + e)
+            console.error('error getting data' + e);
         }
     }
 
-    async getURL(){
-        return await this.getData('apod').then(r => r.url)
+    async getTitle() {
+        return await this.getData('apod').then(r => r.title);
     }
 
-    // updateContent = (e) => {
-    //     this.setState({
-    //         copyright: e.copyright,
-    //         date: e.date,
-    //         explanation: e.explanation,
-    //         hdurl: e.hdurl,
-    //         title: e.title,
-    //         url: e.url
-    //     })
-    // }
+    async getDate() {
+        return await this.getData('apod').then(r => r.date);
+    }
+
+    async getCopyright() {
+        return await this.getData('apod').then(r => r.copyright);
+    }
+
+    async getURL() {
+        return await this.getData('apod').then(r => r.url);
+    }
+
+    async getExplanation() {
+        return await this.getData('apod').then(r => r.explanation);
+    }
+
+    async getHDURL() {
+        return await this.getData('apod').then(r => r.hdurl);
+    }
 
     async fetchPicture() {
         await fetch(
@@ -96,30 +90,59 @@ export default class NPOD extends Component {
             .then(response => response.json())
             .then(responseJson => {
                 this.storeData('apod', responseJson);
-                // this.updateContent(responseJson)
-                // console.log(responseJson)
-                // console.log(this.getData('apod'))
-                // console.log(this.state.explanation);
-                // console.log(this.state.url);
             })
-            .catch(error => console.log('apod ' + error));
-            console.warn(await this.getURL())
+            .catch(error => console.error('fetch error ' + error));
     }
 
     render() {
         return (
-            <ScrollView /* style={{width: 300, height: 300}} */>
-                <Image style={styles.image} source={{uri: this.state.url}} />
-                {/* <Text>{this.getData('apod').url}</Text> */}
-                {/* <Text>{this.getData('apod').explanation}</Text> */}
+            <ScrollView style={{marginLeft: 10}}>
+                <View>
+                    <Text style={styles.title}>{this.state.title}</Text>
+                    <Text style={styles.date}>{this.state.date}</Text>
+                    <Text style={styles.date}>{this.state.copyright}</Text>
+                    <View style={styles.block}>
+                        <Image
+                            style={styles.image}
+                            source={{uri: this.state.url}}
+                        />
+                    </View>
+                    <Text style={styles.explanation}>
+                        {this.state.explanation}
+                    </Text>
+                    <Text
+                        style={styles.hdurl}
+                        onPress={() => Linking.openURL(`${this.state.hdurl}`)}>
+                        HD Image
+                    </Text>
+                </View>
             </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    title: {
+        color: 'black',
+        fontSize: 25,
+    },
+    date: {
+        color: 'black',
+        fontSize: 20,
+    },
     image: {
+        alignSelf: 'center',
+        marginRight: 10,
         height: 300,
         width: 300,
     },
-})
+    explanation: {
+        color: 'black',
+        fontSize: 20,
+    },
+    hdurl: {
+        alignSelf: 'center',
+        color: 'blue',
+        fontSize: 20,
+    },
+});
